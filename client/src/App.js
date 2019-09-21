128a10e3c8e205c742f7c64aad974cbb8976f707
Ryan Sage <ryansage09@gmail.com>
2019-09-21 17:45:42 -0400

    removed old reference to testing tictactoe game

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React, {Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-//import TicGame from './components/game.js'
+
 import CardsLogic from './components/cardLogic'
 
 class App extends Component {
   render () {
     return (
       <div>
-        {/* <TicGame /> */}
         <CardsLogic />
       </div>
     )
