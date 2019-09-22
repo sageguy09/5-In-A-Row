d9b4868bed15b07b9d0a66bc50231af9687cd65b
Ryan Sage <ryansage09@gmail.com>
2019-09-22 09:34:55 -0400

    refactored gameboard and logic to show the FIR gameboard, added styling, his the cell props in div

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, {Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import CardsLogic from './components/cardLogic'
+import CardsLogic from './components/gameLogic'
 
 class App extends Component {
   render () {
--- client/src/components/gameBoard.js
+++ client/src/components/gameBoard.js
@@ -1,60 +1,59 @@
 import React, {Component} from 'react';
 
 class gameBoard extends Component {
-    onClick(id) {
-        if (this.isActive(id)) {
-            this.props.moves.clickCell(id);
-            this.props.events.endTurn();
-        }
-    }
 
-    isActive(id) {
+    onClick = id => {
+        if (this.isActive(id)) {
+          this.props.moves.clickCell(id);
+        }
+      };
+      //write function that translates cardId to be cell[id]
+      //when playCard is ran, it calls to clickCell and places id
+      isActive(id) {
         if (!this.props.isActive) return false;
         if (this.props.G.cells[id] !== null) return false;
         return true;
-    }
-
-    render() {
-        let winner = '';
-        if (this.props.ctx.gameover) {
-            winner = 
-            this.props.ctx.gameover.winner !==undefined ? (
-                <div id="winner">Winner: {this.props.ctx.gameover.winner}</div>
-            ) : (
-                <div id="winner">Draw!</div>
-            );
-        }
-
-        const cellStyle = {
-            border: '1px solid #555',
-            width: '50px',
-            height: '50px',
-            lineHeight:'50px',
-            textAlign: 'center',
-        };
-
+      }
+    
+      render() {
         let tbody = [];
         for (let i = 0; i < 10; i++) {
-            let cells = [];
-            for (let j = 0 ; j < 10; j++) {
-                const id = 10 * i + j;
-                cells.push(
-                    <td style={cellStyle} key={id} onClick={() => this.onClick(id)}>
-                        {this.props.G.cells[id]}
-                    </td>
-                );
-            }
-            tbody.push(<tr key={i}>{cells}</tr>);
+          let cells = [];
+          for (let j = 0; j < 10; j++) {
+            const id = 10 * i + j;
+            cells.push(
+              <td
+                key={id}
+                className={this.isActive(id) ? 'active' : ''}
+                onClick={() => this.onClick(id)}
+              >
+                {this.props.G.board.boardArray[id]}
+                <div hidden>{this.props.G.cells[id]}</div>
+              </td>
+            );
+          }
+          tbody.push(<tr key={i}>{cells}</tr>);
         }
+    
+        let winner = null;
+        if (this.props.ctx.gameover) {
+          winner =
+            this.props.ctx.gameover.winner !== undefined ? (
+              <div id="winner">Winner: {this.props.ctx.gameover.winner}</div>
+            ) : (
+                <div id="winner">Draw!</div>
+              );
+        }
+        
         return (
-            <div>
-                <table id="board">
-                    <tbody>{tbody}</tbody>
-                </table>
-                {winner}
-            </div>
+          <div>
+            <table id="board">
+              <tbody>{tbody}</tbody>
+            </table>
+            {winner}
+          </div>
         );
+      }
     }
-}
 
 export default gameBoard;
