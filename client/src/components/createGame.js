d81556d337f399f8b4ea15280d0ee597644213d3
Ryan Sage <ryansage09@gmail.com>
2019-09-23 11:26:53 -0400

    added basic layout of createGame form

--- client/src/components/createGame.js
+++ client/src/components/createGame.js
@@ -7,11 +7,25 @@ class gameCreate extends React.Component {
         return (
         <div>
             <h1>Create Game</h1>
-            <select name="users" id="userSelect" default="select player">
+
+            <form action="#" id="createGame">
+                Game Name: <input type="text" name="gameName"></input>
+            </form>
+            Select Player 1: <select name="users" form="createGame" id="userSelect" default="select player">
             <option value="none" selected disabled hidden>Select a User</option>
             <option value="player_1">Player 1</option>
             <option value="player_2">Player 2</option>
             </select>
+            <br/>
+            Select Player 2: <select name="users" form="createGame" id="userSelect" default="select player">
+            <option value="none" selected disabled hidden>Select a User</option>
+            <option value="player_1">Player 1</option>
+            <option value="player_2">Player 2</option>
+            </select>
+            <br />
+            <input type="submit" form="creaetGame" value="Create Game" />
+            <br />
+            <button>Add a player to the database</button>
             {/* {userListing(this.state.users)}
             {this.state.users.map(users => (
                 userComp(users)
