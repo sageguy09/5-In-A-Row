8ffbfe6ba4773c31e9c7077aa66563be29101094
Ryan Sage <ryansage09@gmail.com>
2019-09-24 00:51:07 -0400

    successfully called CardsLogic to Home. set App.js to call Home.

---
+++ client/src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+/*import {Route, Switch} from 'react-router-dom'
+
+import Users from './components/users'
+import User from './components/user'
+import AddUserForm from './components/addUser'
+import CreateGame from './components/createGame'*/
+import CardsLogic from './gameLogic.js'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <CardsLogic />
+            </div>
+        )
+    }
+}
+
+export default Home;
